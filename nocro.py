04c3a22fee93c13121adcb7f8e9a5c4709dba1c5
Kor-NOH <shehddnjs1982@naver.com>
2022-12-23 18:56:52 +0900

    macro base

---
+++ nocro.py
@@ -0,0 +1,12 @@
+import pyautogui
+from tkinter import *
+
+while(1):
+    def press(event):
+        print(event)
+        
+#pyautogui.moveTo(743, 178, 2)
+# 화면상 x=100, y = 100으로 마우스가 2초간 이동함
+
+#while(1):
+#    pyautogui.doubleClick()
